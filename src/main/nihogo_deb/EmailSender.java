e9d906cf877326a624329c5dd080ccd61eac0687
nihongo-deb <dal34@tpu.ru>
2022-06-30 19:00:33 +0700

    create my working directory, add new dependencies to wmail work

---
+++ src/main/nihogo_deb/EmailSender.java
@@ -0,0 +1,7 @@
+/**
+ * @author KAWAIISHY
+ * @project BirthdayNotificationer
+ * @created 30.06.2022
+ */
+public class EmailSender {
+}
---
+++ src/main/nihogo_deb/RunClass.java
@@ -0,0 +1,10 @@
+/**
+ * @author KAWAIISHY
+ * @project BirthdayNotificationer
+ * @created 30.06.2022
+ */
+public class RunClass {
+    public static void main(String[] args) {
+
+    }
+}
